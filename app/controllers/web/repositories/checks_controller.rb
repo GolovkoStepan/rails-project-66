30fbd9bf62a458210cd1ed12570ba148ca7ce40d
GolovkoStepan <stepagolovko1993@gmail.com>
2025-04-17 14:01:37 +0500

    Небольшие доработки.

--- app/controllers/web/repositories/checks_controller.rb
+++ app/controllers/web/repositories/checks_controller.rb
@@ -2,18 +2,26 @@
 
 class Web::Repositories::ChecksController < Web::ApplicationController
   before_action :authenticate_user!
+  before_action :set_repository
 
   def show
-    @check    = Repository::Check.includes(:repository).find(params[:id])
-    @offenses = @check.offenses.page(params[:page]).per(30)
+    if (@check = @repository.checks.find_by(id: params[:id]))
+      @offenses = @check.offenses.page(params[:page]).per(30)
+    else
+      redirect_to repository_path(@repository), alert: t('.not_found')
+    end
   end
 
   def create
-    repository = Repository.find(params[:repository_id])
-    CheckRepositoryJob.perform_async(repository.id)
-
-    redirect_to repository_path(repository), notice: t('.success')
+    CheckRepositoryJob.perform_async(@repository.id)
+    redirect_to repository_path(@repository), notice: t('.success')
   rescue SidekiqUniqueJobs::Conflict
-    redirect_to repository_path(repository), alert: t('.already_in_progress')
+    redirect_to repository_path(@repository), alert: t('.already_in_progress')
+  end
+
+  private
+
+  def set_repository
+    @repository = current_user.repositories.find(params[:repository_id])
   end
 end
